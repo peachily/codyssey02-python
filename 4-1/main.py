c61d9cb9f7345150aa70a5d2739575f25fec4b3d
peachily <peachily1108@gmail.com>
2025-09-19 20:35:06 +0900

    [4-1] log파일 list로 만들기

--- 4-1/main.py
+++ 4-1/main.py
@@ -1,1 +1,13 @@
-file = open('mission_computer_main.log', 'r', encoding = 'utf-8')
+try:
+  with open('mission_computer_main.log', 'r', encoding = 'utf-8') as f:
+    log = f.read()
+    log_lines = log.splitlines()
+    log_comma = [line.split(',') for line in log_lines]
+  print(log_comma)
+
+except FileNotFoundError:
+  print()
+except UnicodeDecodeError:
+  print()
+except Exception as e:
+  print('오류 내용: ', e)
