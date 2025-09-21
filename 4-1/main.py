08017d6b3cee65df45e9e7804b059fffb77acc38
peachily <peachily1108@gmail.com>
2025-09-21 21:25:33 +0900

    [4-1] json 파일로 저장하기

--- 4-1/main.py
+++ 4-1/main.py
@@ -1,13 +1,21 @@
+import json
+
 try:
   with open('mission_computer_main.log', 'r', encoding = 'utf-8') as f:
     log = f.read()
     log_lines = log.splitlines()
     log_comma = [line.split(',') for line in log_lines]
-  print(log_comma)
+    header = log_comma[0]
+    log_data = log_comma[1:]
+    log_data.sort(key= lambda x: x[0], reverse=True)
+    mission_computer = [dict(zip(header, data)) for data in log_data]
+
+  with open ('mission_computer_main.json', 'w', encoding='utf-8') as f:
+    json.dump(mission_computer, f, ensure_ascii=False, indent=4)
 
 except FileNotFoundError:
-  print()
+  print('파일을 찾을 수 없습니다')
 except UnicodeDecodeError:
-  print()
+  print('파일 인코딩 문제 발생')
 except Exception as e:
   print('오류 내용: ', e)
